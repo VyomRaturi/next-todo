43bb3138269b6783a5be2eef72973759846a6cad
Vyom Raturi <vyomraturi2004@gmail.com>
2024-05-10 14:59:18 +0530

    add todo action

--- components/shared/AddTodo.tsx
+++ components/shared/AddTodo.tsx
@@ -1,10 +1,11 @@
+import { create } from "@/app/actions/todoActions";
 import Form from "../ui/Form";
 import Input from "../ui/Input";
 import Button from "../ui/Button";
 
 const AddTodo = () => {
   return (
-    <Form className="w-1/2 m-auto">
+    <Form action={create} className="w-1/2 m-auto">
       <div className="flex">
         <Input name="input" type="text" placeholder="Add Todo..." />
         <Button type="submit" text="Add" />
